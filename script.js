450d9b64c7d4decfb61a628a81e534f521925ba9
Tamerlan2006 <241648@astanait.edu.kz>
2025-12-26 00:04:52 +0500

    feat: partialy done: weather is being displayed

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+const dotenv = require('dotenv');
 const express = require('express');
+dotenv.config();
+
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const app = express();
 const port = 3000;
-const API_KEY = process.env.OPENWEATHER_API
+const CITY = "Astana";
+const API_KEY = process.env.OPENWEATHER_API;
+const URL = "https://api.openweathermap.org/data/2.5/weather";
 
-app.get('/', (req, res) => {
-    
-})
+async function getWeatherByCity(city) {
+    const params = new URLSearchParams({
+        q: city,
+        appid: API_KEY,
+        units: "metric"
+    });
 
-app.listen(port, ()=> {
-    console.log("http://localhost:3000")
-});
+    const response = await fetch(`${URL}?${params.toString()}`);
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+    console.log(`Weather in ${data.name}`);
+    console.log(`Temperature: ${data.main.temp} °C`);
+    console.log(`Feels like: ${data.main.feels_like} °C`);
+}
+// getWeatherByCity(CITY);
